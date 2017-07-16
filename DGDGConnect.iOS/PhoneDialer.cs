3c699e2ae3c8b011ed1683a0582fa7535dfad704
Harry Martin <HMHAMz@gmail.com>
2017-07-16 19:16:46 +1000

    Initial commit

---
+++ DGDGConnect.iOS/PhoneDialer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using DGDGConnect.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace DGDGConnect.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial (string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ DGDGConnect/PhoneTranslator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class PhoneTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach(var c in raw)
+            {
+                if ("-0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                    //Bad character entered?
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0; //returns 1 or 0 depending on whether the char 'c' is a member of keyString
+        }
+
+        static readonly String[] digits = {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                    return 2 + i; // add two because the numbers zero and 1 do not contain letters on the dialpad
+            }
+            return null;
+        }
+    }
+}
